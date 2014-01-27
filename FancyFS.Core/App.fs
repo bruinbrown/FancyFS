d8dd3ddade4375c6c14a1f64d9d3e76a75b612a9
Anthony Brown <bruinbrown@gmail.com>
2014-01-27 16:29:43 +0000

    Started to work on pipelining a request

--- FancyFS.Core/App.fs
+++ FancyFS.Core/App.fs
@@ -20,3 +20,27 @@ type Response =
         StatusCode : System.Net.HttpStatusCode option
         Body : string
     }
+
+type Pipeline =
+    {
+        Element : (Request -> Response -> Response)
+        Next : Pipeline option
+    }
+
+module App =
+    
+    let private defaultResponse = { Headers = Map.empty; StatusCode = None; Body = ""; }
+
+    let (==>) pipeline func =
+        let p = { Element = func; Next = None; }
+        { pipeline with Next = Some p; }
+
+    let ExecutePipeline pipeline request =
+        let rec ExecuteElement req resp elem =
+            let r = elem.Element req resp
+            match r.StatusCode with
+            | Some _ -> r
+            | None -> match elem.Next with
+                      | Some x -> x.Element req r
+                      | None -> r
+        ExecuteElement request defaultResponse pipeline 
