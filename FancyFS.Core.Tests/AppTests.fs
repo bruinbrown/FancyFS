1fb17a64d18ebc3cfd7381925a99e4f631ca258d
Anthony Brown <bruinbrown@gmail.com>
2014-01-27 17:22:30 +0000

    Started writing tests

---
+++ FancyFS.Core.Tests/AppTests.fs
@@ -0,0 +1,28 @@
+﻿namespace FancyFS.Core
+
+open FsUnit
+open NUnit.Framework
+
+[<TestFixture>]
+module AppTests =
+
+    open FancyFS.Core
+    open FancyFS.Core.App
+
+    let private PipelineLength pipeline =
+        let rec Count pipeline count =
+            match pipeline.Next with
+            | None -> count
+            | Some x -> Count x (count+1)
+        Count pipeline 0
+
+    [<Test>]
+    let ``Using ==> should increase pipeline length`` () =
+        let testFun = fun req resp -> resp
+
+        let emptyPipeline = BaseRequest
+
+        let newPipeline = emptyPipeline ==> testFun
+
+        (PipelineLength emptyPipeline) |> should equal 0
+        (PipelineLength newPipeline) |> should equal 1
