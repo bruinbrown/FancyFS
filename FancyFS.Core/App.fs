3c9eecb4b1d82c33cc87d0163103f5f3e04120fc
Anthony Brown <bruinbrown@gmail.com>
2014-01-27 16:25:35 +0000

    Added the key elements of a request and response

---
+++ FancyFS.Core/App.fs
@@ -0,0 +1,22 @@
+﻿namespace FancyFS.Core
+
+open Microsoft.FSharp.Collections
+
+type IUser =
+    interface
+    end
+
+type Request =
+    {
+        Headers : Map<string, string>
+        QueryString : Map<string, string>
+        Path : System.Uri
+        User : IUser option
+    }
+
+type Response =
+    {
+        Headers : Map<string, string>
+        StatusCode : System.Net.HttpStatusCode option
+        Body : string
+    }
