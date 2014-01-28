27437c5a8dd2762c1aff00a1240cce83676dc249
Anthony Brown <bruinbrown@gmail.com>
2014-01-28 23:04:48 +0000

    Branching managed to mess up the projects, should be fixed now

---
+++ FancyFS.Router/Router.fs
@@ -0,0 +1,12 @@
+﻿namespace FancyFS.Routing
+
+open FancyFS.Core
+open FancyFS.Core.PipelineModule
+
+module Router =
+
+    let Router routeDictionary input =
+        input
+
+    let CreateRouteDictionary routes =
+        ()
