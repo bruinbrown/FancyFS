f263cfc9bd0d1cfae15ffcc49d8420aca8320d75
Anthony Brown <bruinbrown@gmail.com>
2014-01-28 12:32:15 +0000

    Updated the web project to show a basic implementation

---
+++ FancyFS.ASP.Example/app.fs
@@ -0,0 +1,14 @@
+﻿namespace FancyFS.ASP.Example
+
+open FancyFS.Core
+open FancyFS.Core.App
+
+type ExamplePipelineLocation () =
+
+    let writerFunc = fun (req, res) -> (req, { res with Body = "testing"})
+
+    let pipeline = BaseRequest ==> writerFunc
+
+    interface IPipelineLocation with
+        member this.Pipeline
+            with get () = pipeline
