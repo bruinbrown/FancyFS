19c353d075f60a8858024f5540470575aaf6087c
Anthony Brown <bruinbrown@gmail.com>
2014-02-24 15:54:59 +0000

    Added some little functions to make life easier

---
+++ FancyFS.Router.RouteTypeProvider/Utilities.fs
@@ -0,0 +1,26 @@
+﻿namespace FancyFS.Routing
+
+[<AutoOpen>]
+module Utilities =
+
+    open FancyFS.Routing.RouteParsers
+
+    let mdict (sequence:('a * 'b) seq) =
+        let dict = System.Collections.Generic.Dictionary<'a, 'b>()
+        sequence
+        |> Seq.iter (fun (a, b) -> dict.[a] <- b)
+        dict
+
+    let Parse (pattern:string) (arg:string) =
+        let d = Seq.zip (pattern.Split('/')) (arg.Split('/'))
+                |> Seq.map (fun (f,s) -> f.TrimStart('{').TrimEnd('}'), s)
+                |> Seq.map (fun (f,s) -> let q = f.Split(':')
+                                         if q.Length = 1 then
+                                             f, q.[0] :> obj
+                                         else
+                                             let convname = q.[1]
+                                             let res = ConvertItem s q.[1]
+                                             q.[0], res)
+                |> mdict
+        d
+
