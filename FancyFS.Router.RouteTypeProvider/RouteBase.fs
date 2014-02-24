e8554c95a09f226a8dc769f30e0ff9aa942f1335
Anthony Brown <bruinbrown@gmail.com>
2014-02-24 15:15:09 +0000

    Added base type for all provided routes

---
+++ FancyFS.Router.RouteTypeProvider/RouteBase.fs
@@ -0,0 +1,12 @@
+﻿namespace FancyFS.Routing
+
+open System.Collections.Generic
+
+type RouteBase(pars:Dictionary<string, obj>) as this =
+
+    [<DefaultValue>] val mutable private routeParameters : Dictionary<string, obj>
+
+    do this.routeParameters <- pars
+
+    new () =
+        RouteBase(Dictionary<string, obj>())
